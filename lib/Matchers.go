dea077724e24d1b36280dca79dd68095e60cc7f0
kun <1196990763@qq.com>
2021-09-28 21:08:33 +0800

    超级管理员

--- lib/Matchers.go
+++ lib/Matchers.go
@@ -14,11 +14,28 @@ func init()  {
 		}
 		return nil,errors.New("MethodMatch error")
 	})
+	E.AddFunction("SuperMatch", func(arguments ...interface{}) (interface{}, error) {
+		if len(arguments) == 1 {
+			user := arguments[1].(string)
+			return SuperMatch(user),nil
+		}
+		return nil,errors.New("SuperMatch error")
+	})
 }
 
 func MethodMatch(key1, key2 string) bool {
 	if key1==key2 {
 		return true
+	}
+	return false
+}
+// 这里我写死了，实际可以从数据库中读取
+var roots = []string{"root","admin"}
+func SuperMatch(userName string) bool {
+	for _,user := range roots {
+		if user==userName {
+			return true
+		}
 	}
 	return false
 }
